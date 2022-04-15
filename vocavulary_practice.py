5dacb2c9665ef3e1e627028dc866a7dc982f03a1
josueNoha <nohajosue@gmail.com>
2022-04-15 16:02:05 -0500

    modificaciones vocavulary practice

--- vocavulary_practice.py
+++ vocavulary_practice.py
@@ -21,7 +21,8 @@ def order_words():
     
     return [num_answer, num_1, num_2]
 
-def print_words(answer,word_1,word_2,order):
+def print_english_words(answer,word_1,word_2,order):
+    # imprime la palabra en inglés, las alternativas y comprueba si la respuesta es correcta
     words_dict = {order[0]:answer, order[1]:word_1, order[2]:word_2}
     print('Palabra en ingles: ')
     print(answer)
@@ -35,6 +36,65 @@ def print_words(answer,word_1,word_2,order):
         print('\n Respuesta correcta')
     else:
         print('\n Respuesta incorrecta, la respuesta correcta es:\n {}'.format(vocavulary[answer]))
+
+def print_spanish_words(answer,word_1,word_2,order):
+    # imprime la palabra en español, las alternativas y comprueba si la respuesta es correcta
+    words_dict = {order[0]:answer, order[1]:word_1, order[2]:word_2}
+    print('Palabra en español: ')
+    print(vocavulary[answer])
+    print('\n Alternativas:')
+    for i in range(3):
+        print("{}) {}".format(i+1,words_dict[i]) )
+
+    m = int(input('\n Elija la opción correcta: '))
+
+    if words_dict[m-1] == answer:
+        print('\n Respuesta correcta')
+    else:
+        print('\n Respuesta incorrecta, la respuesta correcta es:\n {}'.format(answer))
+
+def select_words():
+    #elige tres palabras para jugar, la primera es la respuesta correcta
+    answer = choose_word(vocavulary)
+    
+    word_1 = choose_word(vocavulary)
+
+    while word_1 == answer:
+        word_1 = choose_word(vocavulary)
+
+    word_2 = choose_word(vocavulary)
+
+    while word_2 == answer or word_2 ==word_1:
+        word_2 = choose_word(vocavulary)
+
+    return answer, word_1, word_2
+
+
+def elegir_modo():
+    #permite elegir el modo de juego, se repite hasta tener una respuesta correcta
+    mode = input('\n Elija 1 si desea jugar con palabras en inglés, 2 si desea jugar con palabras en español o 0 si desea finalizar: ')
+
+    while mode not in ['0','1','2']:
+        mode = input('\n Elija 1 si desea jugar con palabras en inglés, 2 si desea jugar con palabras en español o 0 si desea finalizar: ')
+
+    return mode
+
+def main():
+    #programa principal, elige un modo de juego y se repite hasta elegir 0
+    mode = elegir_modo()
+    answer, word_1, word_2 = select_words()
+    order = order_words()
+
+    while mode in ['1','2']:
+        if mode == '1':
+            print_english_words(answer,word_1,word_2,order)
+
+        elif mode == '2':
+            print_spanish_words(answer,word_1,word_2,order)
+
+        mode = elegir_modo()
+        answer, word_1, word_2 = select_words()
+        order = order_words()
 
 vocavulary = {
     'crispness' : 'frescura',
@@ -109,17 +169,4 @@ vocavulary = {
 
 
 if __name__ == '__main__':
-    answer = choose_word(vocavulary)
-    
-    word_1 = choose_word(vocavulary)
-
-    while word_1 == answer:
-        word_1 = choose_word(vocavulary)
-
-    word_2 = choose_word(vocavulary)
-
-    while word_2 == answer or word_2 ==word_1:
-        word_2 = choose_word(vocavulary)
-
-    order = order_words()
-    print_words(answer,word_1,word_2,order)
+    main()
